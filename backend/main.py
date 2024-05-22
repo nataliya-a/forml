79e09dc88c0fa23471d6a6e17b211a2733ad1185
nataliya-a <nkarmarkar@dons.usfca.edu>
2024-05-21 22:52:49 -0700

    chore: Enable CORS for all routes in backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from crack_safe import crack_safe
 
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
+# cors = CORS(app, resources={r"/foo": {"origins": "*"}})
+# set the CORS origins to allow all
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route("/")
@@ -13,6 +16,7 @@ def hello_world():
 
 @app.route("/api/crack_safe", methods=['POST'])
 def crack_safe_api():
+    print("here")
     data = request.json
     actual_combination = data['actual_combination']
     attempts, runtime = crack_safe(actual_combination)
